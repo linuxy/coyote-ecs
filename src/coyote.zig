9b1fd2b9fd1a893874cbd7100e94793f23581d39
Ian Applegate <ian@jedi.sh>
2022-06-10 09:56:49 -0400

    fixes & cleanup

--- src/coyote.zig
+++ src/coyote.zig
@@ -26,12 +26,9 @@ pub fn main() !void {
     //Create a world
     var world = World.create();
 
-    //Destroy all components, entities and the world at end of scope
-    //defer world.deinit();
-
     //Create an entity
-    var anOrange = world.entities.create();
-    var anApple = world.entities.create();
+    var anOrange = try world.entities.create();
+    var anApple = try world.entities.create();
     std.log.info("Created an Orange ID: {}", .{anOrange.id});
 
     //Create a unique component
@@ -45,7 +42,7 @@ pub fn main() !void {
     //Create 20k entities and attach 20k unique components
     var i: usize = 0;
     while(i < 20000) : (i += 1) {
-        var anEntity = world.entities.create();
+        var anEntity = try world.entities.create();
         var anOrangeComponent = try world.components.create(Comp.Orange{});
         try anEntity.attach(anOrangeComponent, Comp.Orange{.color = 1, .ripe = false, .harvested = false});
     }
@@ -60,6 +57,7 @@ pub fn main() !void {
 
     Systems.run(Grow, .{world});
     Systems.run(Harvest, .{world});
+    Systems.run(Raze, .{world});
 
     std.log.info("Entities: {}", .{world.entities.count()});
     std.log.info("Components: {}", .{world.components.count()});
@@ -80,11 +78,12 @@ pub const Components = struct {
     pub const Iterator = struct {
         ctx: *const Components,
         index: usize = 0,
+        alive: u32 = 0,
 
         pub fn next(it: *Iterator) ?*Component {
             if (it.ctx.alive == 0) return null;
 
-            const end = it.ctx.alive;
+            const end = it.alive;
             var metadata = it.index;
 
             while (metadata < end) : ({
@@ -106,6 +105,7 @@ pub const Components = struct {
         ctx: *const Components,
         index: usize = 0,
         filter_type: u32,
+        alive: u32 = 0,
 
         pub fn next(it: *MaskedIterator) ?*Component {
             if (it.ctx.alive == 0) return null;
@@ -133,10 +133,21 @@ pub const Components = struct {
     pub inline fn create(ctx: *Components, comp_type: anytype) !*Component {
         var world = @ptrCast(*World, @alignCast(@alignOf(World), ctx.world));
 
+        if(ctx.alive >= MAX_COMPONENTS)
+            return error.NoFreeComponentSlots;
+
         //find end of sparse array
-        //TODO: wraparound from end to free_idx
-        while(ctx.sparse_data[ctx.free_idx].allocated == true)
+        var wrapped = false;
+        while(ctx.sparse_data[ctx.free_idx].allocated == true) {
+            if(wrapped and ctx.free_idx > MAX_COMPONENTS)
+                return error.NoFreeComponentSlots;
+
             ctx.free_idx = ctx.alive + 1;
+            if(ctx.free_idx > MAX_COMPONENTS - 1) {
+                ctx.free_idx = 0;
+                wrapped = true;
+            }
+        }
         
         var component = &ctx.sparse_data[ctx.free_idx];
 
@@ -149,7 +160,6 @@ pub const Components = struct {
 
         ctx.sparse[ctx.free_idx] = component;
 
-        //std.log.info("Created component of ID: {}", .{component.id});
         ctx.free_idx += 1;
         ctx.created += 1;
         ctx.alive += 1;
@@ -163,74 +173,18 @@ pub const Components = struct {
     pub inline fn count(ctx: *Components) u32 {
         //count of all living components
 
-        //std.log.info("Components alive: {}", .{ctx.len});
         return ctx.alive;
     }
 
     pub inline fn iterator(self: *const Components) Iterator {
-        return .{ .ctx = self };
+        return .{ .ctx = self, .alive = self.alive };
     }
 
     pub inline fn iteratorFilter(self: *const Components, comp_type: anytype) Components.MaskedIterator {
         //get an iterator for components attached to this entity
         return .{ .ctx = self,
-                  .filter_type = typeToId(comp_type) };
-    }
-
-    pub fn sparse_resize(ctx: *Components) void {
-        if(ctx.resized > 0) {
-            ctx.sparse = allocator.realloc(ctx.sparse, MAX_ENTITIES * (ctx.resized + 1)) catch unreachable;
-            var idx: usize = MAX_ENTITIES * ctx.resized;
-            while(idx < (MAX_ENTITIES * (ctx.resized + 1))) {
-                ctx.sparse[idx] = allocator.create(Component) catch unreachable;
-                idx += 1;
-            }
-            std.log.info("Realloc components from: {} to {}", .{MAX_ENTITIES * ctx.resized, MAX_ENTITIES * (ctx.resized + 1)});
-        } else {
-            ctx.sparse = allocator.alloc(*Component, MAX_ENTITIES) catch unreachable;
-            var idx: usize = 0;
-            while(idx < MAX_ENTITIES) {
-                ctx.sparse[idx] = allocator.create(Component) catch unreachable;
-                idx += 1;
-            }
-        }
-
-        ctx.resized += 1;
-        ctx.len = MAX_ENTITIES * ctx.resized;
-        std.log.info("Resized components to len: {}", .{ctx.len});
-    }
-
-    pub fn deinit(ctx: *Components) void {
-        var i: usize = 0;
-        var idata: usize = 0;
-        var it = ctx.dense.iterator();
-        while(it.next()) |component| {
-            inline for (@typeInfo(@import("root")).Struct.decls) |decl| {
-                const comp_eql = comptime std.mem.eql(u8, decl.name, COMPONENT_CONTAINER);
-                if (decl.is_pub and comptime comp_eql) {
-                    inline for (@typeInfo((@field(@import("root"), decl.name))).Struct.decls) |member, did| {
-                        if(comptime !std.mem.eql(u8, member.name, "create") and !std.mem.eql(u8, member.name, "deinit") and !std.mem.eql(u8, member.name, "world")
-                        and !std.mem.eql(u8, member.name, "sparse_resize") and !std.mem.eql(u8, member.name, "count")) {
-                            if(component.value_ptr.* != null and did == component.value_ptr.*.?) {
-                                var member_type = @field(@field(@import("root"), decl.name), member.name){};
-                                _ = member_type;
-                                //std.log.info("matched component in deinit() {s} x {}", .{member.name, ctx.sparse[component.key_ptr.*]});
-                                allocator.destroy(Cast(@TypeOf(member_type)).get(ctx.sparse[component.key_ptr.*]).?);
-                                idata += 1;
-                            }
-                        }
-                    }
-                }
-            }
-            allocator.destroy(ctx.sparse[component.key_ptr.*]);
-            i += 1;
-        }
-        while(i < ctx.len) {
-            allocator.destroy(ctx.sparse[i]);
-            i += 1;
-        }
-        std.log.info("Destroyed {} components and {} data", .{i, idata});
-        ctx.dense.deinit();
+                  .filter_type = typeToId(comp_type),
+                  .alive = self.alive };
     }
 };
 
@@ -271,10 +225,22 @@ pub fn Harvest(world: *World) void {
                 i += 1;
             }
         }
-        //component.destroy();
+        component.destroy();
     }
     
     std.log.info("Fruits harvested: {}", .{i});
+}
+
+pub fn Raze(world: *World) void {
+    var it = world.entities.iterator();
+    var i: u32 = 0;
+
+    while(it.next()) |entity| {
+        entity.destroy();
+        i += 1;
+    }
+
+    std.log.info("Entities destroyed: {}", .{i});
 }
 
 const World = struct {
@@ -295,20 +261,14 @@ const World = struct {
                                       .sparse_data = undefined,
                                       .world = world,
                                       .len = 0,
-                                      .alive = 0,};
+                                      .alive = 0,
+                                      };
         var i: usize = 0;
         while(i < componentCount()) {
-            std.log.info("Component index: {}", .{i});
             world.entities.component_mask[i] = std.StaticBitSet(MAX_COMPONENTS).initEmpty();
             i += 1;
         }
         return world;
-    }
-
-    pub fn deinit(ctx: *World) void {
-        ctx.components.deinit();
-        ctx.entities.deinit();
-        allocator.destroy(ctx);
     }
 };
 
@@ -321,7 +281,7 @@ const Component = struct {
     typeId: ?u32 = undefined,
     allocated: bool = false,
     alive: bool = false,
-
+    
     pub inline fn is(self: *const Component, comp_type: anytype) bool {
         if(self.typeId == typeToId(comp_type)) {
             return true;
@@ -362,13 +322,15 @@ const Component = struct {
     pub inline fn destroy(self: *Component) void {
         var world = @ptrCast(*World, @alignCast(@alignOf(World), self.world));
 
+        world.entities.component_mask[@intCast(usize, self.typeId.?)].setValue(self.id, false);
+
         self.data = null;
         self.attached = false;
         self.owner = null;
         self.typeId = null;
         self.allocated = false;
         self.alive = false;
-
+        
         world.components.alive -= 1;
         world.components.free_idx = self.id;
     }
@@ -401,7 +363,8 @@ const Entity = struct {
         component.attached = true;
         component.owner = self.id;
         component.typeId = typeToId(comp_type);
-
+        component.allocated = true;
+        
         world.entities.component_mask[@intCast(usize, component.typeId.?)].setValue(component.id, true);
     }
 
@@ -412,7 +375,10 @@ const Entity = struct {
     }
 
     pub inline fn destroy(self: *Entity) void {
+        var world = @ptrCast(*World, @alignCast(@alignOf(World), self.world));
+
         self.alive = false;
+        world.entities.alive -= 1;
     }
 
     pub fn get(self: *Entity, comptime T: type) type {
@@ -421,13 +387,14 @@ const Entity = struct {
     }
 
     pub inline fn iterator(self: *const Entities) Entities.Iterator {
-        return .{ .ctx = self };
+        return .{ .ctx = self, .alive = self.alive };
     }
 
-    pub inline fn filteredIterator(self: *const Entity, comp_type: anytype) Components.MaskedIterator {
+    pub inline fn filteredIterator(self: *Entity, comp_type: anytype) Components.MaskedIterator {
         //get an iterator for components attached to this entity
         return .{ .ctx = self,
-                  .filter_type = typeToId(comp_type) };
+                  .filter_type = typeToId(comp_type),
+                  .alive = self.alive };
     }
 
     pub inline fn set(self: *Entity, component: *Component, comp_type: anytype, members: anytype) !void {
@@ -461,10 +428,8 @@ pub inline fn typeToId(t: anytype) u32 {
             inline for (@typeInfo((@field(@import("root"), decl.name))).Struct.decls) |member| {
                 const comp_idx = comptime std.mem.indexOf(u8, @typeName(@TypeOf(t)), member.name);
                 if(comp_idx != null) {
-                    //std.log.info("typeToId MATCHED idx: {} member.name: {s}", .{idx, member.name});
                     break;
                 }
-                //std.log.info("typeToId UNMATCHED idx: {} member.name: {s} {s}", .{idx, member.name, @typeName(@TypeOf(t))});
                 idx += 1;
             }
         }
@@ -480,7 +445,6 @@ pub inline fn idEqualsType(id: u32, t: anytype) bool {
         if (decl.is_pub and comptime comp_eql) {
             inline for (@typeInfo((@field(@import("root"), decl.name))).Struct.decls) |member| {
                 if(idx == id and std.mem.indexOf(u8, @typeName(t), member.name) != null) {
-                    //std.log.info("Matched idx: {} to id: {} @ {s}", .{idx, id, @typeName(t)});
                     return true;
                 }
                 idx += 1;
@@ -519,7 +483,7 @@ pub inline fn Cast(comptime T: type) type {
 
 const Entities = struct {
     len: u32 = 0,
-    sparse: [MAX_ENTITIES]*const Entity,
+    sparse: [MAX_ENTITIES]*Entity,
     sparse_data: [MAX_ENTITIES]Entity,
     alive: u32 = 0,
     free_idx: u32 = 0,
@@ -531,11 +495,12 @@ const Entities = struct {
     pub const Iterator = struct {
         ctx: *const Entities,
         index: usize = 0,
+        alive: u32 = 0,
 
-        pub fn next(it: *Iterator) ?*const Entity {
+        pub fn next(it: *Iterator) ?*Entity {
             if (it.ctx.alive == 0) return null;
 
-            const end = it.ctx.alive;
+            const end = it.alive;
             var metadata = it.index;
 
             while (metadata < end) : ({
@@ -553,14 +518,21 @@ const Entities = struct {
         }
     };
 
-    pub inline fn create(ctx: *Entities) *Entity {
+    pub inline fn create(ctx: *Entities) !*Entity {
         //most ECS cheat here and don't allocate memory until a component is assigned
 
         //find end of sparse array
-        //TODO: wraparound from end to free_idx
+        var wrapped = false;
+        while(ctx.sparse_data[ctx.free_idx].alive == true) {
+            if(wrapped and ctx.free_idx > MAX_ENTITIES)
+                return error.NoFreeEntitySlots;
 
-        while(ctx.sparse_data[ctx.free_idx].alive == true)
             ctx.free_idx = ctx.alive + 1;
+            if(ctx.free_idx > MAX_ENTITIES - 1) {
+                ctx.free_idx = 0;
+                wrapped = true;
+            }
+        }
 
         var entity = &ctx.sparse_data[ctx.free_idx];
         entity.id = ctx.free_idx;
@@ -571,7 +543,6 @@ const Entities = struct {
         ctx.alive += 1;
         ctx.free_idx += 1;
 
-        //std.log.info("Entities created: {}", .{ctx.free_idx - 1});
         return entity;
     }
 
@@ -582,58 +553,17 @@ const Entities = struct {
                 ctx.sparse[@intCast(usize, ent.id)].alive = false;
                 ctx.free_idx = ent.id;
                 ctx.alive -= 1;
-                //std.log.info("Removed entity: {}", .{ent.id});
             }
         }
     }
 
     pub inline fn iterator(self: *const Entities) Iterator {
-        return .{ .ctx = self };
+        return .{ .ctx = self, .alive = self.alive };
     }
 
     pub inline fn count(ctx: *Entities) u32 {
         //count of all living entities
         return ctx.alive;
-    }
-
-    pub fn sparse_resize(ctx: *Entities) void {
-        if(ctx.entities.resized > 0) {
-            ctx.entities.sparse = allocator.realloc(ctx.entities.sparse, MAX_ENTITIES * (ctx.entities.resized + 1)) catch unreachable;
-            var idx: usize = MAX_ENTITIES * ctx.entities.resized;
-            while(idx < (MAX_ENTITIES * (ctx.entities.resized + 1))) {
-                ctx.entities.sparse[idx] = allocator.create(Entity) catch unreachable;
-                idx += 1;
-            }
-            std.log.info("Realloc from: {} to {}", .{MAX_ENTITIES * ctx.entities.resized, MAX_ENTITIES * (ctx.entities.resized + 1)});
-        } else {
-            ctx.entities.sparse = allocator.alloc(*Entity, MAX_ENTITIES) catch unreachable;
-            var idx: usize = 0;
-            while(idx < MAX_ENTITIES) {
-                ctx.entities.sparse[idx] = allocator.create(Entity) catch unreachable;
-                idx += 1;
-            }
-        }
-
-        ctx.entities.resized += 1;
-        ctx.entities.len = MAX_ENTITIES * ctx.entities.resized;
-        std.log.info("Resized to len: {}", .{ctx.entities.len});
-    }
-
-    pub fn deinit(ctx: *Entities) void {
-        var id: usize = 0;
-        while(id <= ctx.created) {
-            ctx.sparse[id].deinit();
-            id += 1;
-        }
-
-        var idx: usize = 0;
-        while(idx < (MAX_ENTITIES * (ctx.resized))) {
-            allocator.destroy(ctx.sparse[idx]);            
-            idx += 1;
-        }
-
-        ctx.dense.deinit();
-        std.log.info("Destroyed {} entities.", .{idx});
     }
 };
 
